d9c2de3e717bd4a3fa4da208dce39e2f46099652
jasonjabbour <jjj4se@virginia.edu>
2025-02-26 14:06:47 -0500

    enable rviz

--- robots/configs/go2_config/launch/gazebo_velodyne.launch.py
+++ robots/configs/go2_config/launch/gazebo_velodyne.launch.py
@@ -43,7 +43,7 @@ def generate_launch_description():
         description="Use simulation (Gazebo) clock if true",
     )
     declare_rviz = DeclareLaunchArgument(
-        "rviz", default_value="false", description="Launch rviz"
+        "rviz", default_value="true", description="Launch rviz"
     )
     declare_robot_name = DeclareLaunchArgument(
         "robot_name", default_value="go2", description="Robot name"
